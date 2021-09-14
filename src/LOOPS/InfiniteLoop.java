aa0214f71978880f9043f4c36e6fef8824a25ed5
Andrii <andklymyshyn@gmail.com>
2021-09-13 19:10:45 -0500

    I created a new class for homework 8 and I added a new System.out line to infiniteLoop class.

--- src/LOOPS/InfiniteLoop.java
+++ src/LOOPS/InfiniteLoop.java
@@ -4,6 +4,7 @@ public class InfiniteLoop {
     public static void main(String[] args) {
         for ( ; ; ){
             System.out.println("This is infinity LOOP");
+            System.out.println("I didn't save this on GitHub");
         }
         // System.out.println("LOOP"); compile time error
     }
---
+++ src/Project8/Task1.java
@@ -0,0 +1,7 @@
+package Project8;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
